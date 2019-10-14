52ad6b20b3f03a7ec5f24569e4124e6b986f5a6a
zilaiye <q342023971@126.com>
2019-10-14 18:27:12 +0800

    更新订单、支付、商城、测试、用户模块

--- src/router.js
+++ src/router.js
@@ -1,17 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Index.vue'
+import Index from './views/Index.vue'
+
+import article from './views/article/router'
+import goods from './views/goods/router'
+import im from './views/im/router'
+import order from './views/order/router'
+import pay from './views/pay/router'
+import shop from './views/shop/router'
+import store from './views/store/router'
+import test from './views/test/router'
+import user from './views/user/router'
 
 Vue.use(Router)
+
+let routes = [
+  {
+    path: '/',
+    name: 'Index',
+    component: Index
+  },
+]
+
+routes = routes.concat(article)
+routes = routes.concat(goods)
+routes = routes.concat(im)
+routes = routes.concat(order)
+routes = routes.concat(pay)
+routes = routes.concat(shop)
+routes = routes.concat(store)
+routes = routes.concat(test)
+routes = routes.concat(user)
 
 export default new Router({
   // mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-  ]
+  routes: routes
 })
