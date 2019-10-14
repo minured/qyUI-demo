defab58a9040126f3b9774378f6e236cec798247
zilaiye <q342023971@126.com>
2019-10-14 16:24:29 +0800

    引入vant框架

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,38 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <van-tabbar>
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app {
+    background-color: #f5f5f5;
+    font-size: 14px;
   }
-}
+
+  .group {
+    margin-bottom: .2rem;
+  }
+
+  .clear {
+    clear: both
+  }
+
+  .padding {
+    padding: .2rem;
+    background-color: #ffffff;
+  }
+
+  #app .cover {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 99999;
+  }
 </style>
